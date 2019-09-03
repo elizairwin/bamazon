49c3214facb61a0f0e61c246301b1b194ae91d3b
Eliza Irwin <elizairwin@gmail.com>
2019-09-03 13:22:28 -0400

    updated sql file

--- bamazon.sql
+++ bamazon.sql
@@ -6,10 +6,13 @@ CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(255) NOT NULL,
   department_name VARCHAR(255) NOT NULL,
-  price INT default 0,
-  stock_quantity INT default 0,
+  price DECIMAL (10, 2) NOT NULL,
+  stock_quantity INT (10) NOT NULL,
   PRIMARY KEY (id)
 );
 
+--add products in--
 INSERT INTO products (product_name, department_name, price, stock_quantity) 
 VALUES ("Running Sneakers", "Women's Shoes", 100, 25), ("Jersey Swing Dress", "Women's Clothing", 25, 100),  ("Rose Gold Watch", "Women's Jewelry", 300, 25),  ("Shoulder Bag", "Women's Handbags", 150, 10),  ("Aviator Sunglasses", "Women's Accessories", 130, 75),  ("Penny Loafer", "Men's Shoes", 200, 50),  ("Wool Sweater", "Men's Clothing", 35, 20),  ("Leather Belt", "Men's Accessories", 20, 50),  ("Carry-On Luggage", "Luggage", 50, 100),  ("Attaché Case", "Gear", 40, 100);
+
+SELECT * FROM products
